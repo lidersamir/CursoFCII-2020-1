fa044f76d1e9d5290d973fa2ca23f1110a7e745b
crewdark <crewdark@crewdark.crewdark>
2020-02-12 18:20:47 -0500

    segundo commit

--- Documentos/clase0/CC1038417027/suma.cpp
+++ Documentos/clase0/CC1038417027/suma.cpp
@@ -4,17 +4,9 @@
 
 using namespace std;
 
-
-
 int main(){
-	int n1,n2,i,suma;
-	
-	int p;
-	
-	
-	
-	
-	
+	int n1,n2,i,suma,p;
+
 	cout<<"Ingrese el valor inicial de la suma \n";
 	cin>>n1;
 	
@@ -27,27 +19,27 @@ int main(){
 	suma=0;
 	for (i=n1;i<=n2;i+=1){
 		suma=suma+i;
-		//cout<<suma<<endl;
 	}
-
 	
 	if(p==0){
-		
 		printf("El resultado de la suma es: %d \n",suma);
 	}
-	else{
-		ofstream archivo;
 	
-		archivo.open("suma.txt",ios::out);
-		if(archivo.fail()){
-			cout<<"No se puede abrir el archivo";
-			exit(1);
-		}
-		archivo<<"El resultado de la suma es: \n";
-		archivo<<suma;
-		archivo.close();
+	else{//nota: esta parte del guardado no la tengo muy clara 
+		//adjunto lo que hice comentado, me gustaria si profundizar mas en eso 
+		
+		//ofstream archivo;
+	
+		//archivo.open("suma.txt",ios::out);
+		//if(archivo.fail()){
+			//cout<<"No se puede abrir el archivo";
+			//exit(1);
+		//}
+		//archivo<<"El resultado de la suma es: \n";
+		//archivo<<suma;
+		//archivo.close();
+	
 	}
-	//archivo.close; //cerrando el archivo 
 	
 		
 	}
